2cd49a0a2eb6043126f306c6cd8eb20791029bb3
Vitali Muladze <vitalim@rapyd.net>
2023-03-06 22:47:26 +0400

    scrollview

--- react_native/RNCourse/App.js
+++ react_native/RNCourse/App.js
@@ -1,22 +1,75 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { useState } from 'react'
+import { StyleSheet, TextInput, Text, View, Button, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  };
+
+  function addGoalHandler() {
+    setCourseGoals(currentCourseGoals => [...currentCourseGoals, enteredGoalText])
+  };
+
   return (
-    <View style={styles.container}>
-      <View>
-      <Text>mariam miyvarxar</Text>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Your goal"
+          style={styles.textInput}
+          onChangeText={goalInputHandler} />
+
+        <Button title='Add goal' onPress={addGoalHandler} />
       </View>
-      <Text>Hello World!</Text>
-      <Button title="გაგზავნა"/>
+      <View style={styles.goalsContainer}>
+        <ScrollView alwaysBounceVertical={false}>
+          {courseGoals.map((goal) =>
+            <View style={styles.goalItemContainer} key={goal}>
+              <Text style={styles.goalItemText}>{goal}</Text>
+            </View>
+          )}
+        </ScrollView>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#599e46',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 16,
+
   },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 2,
+    borderBottomColor: '#cccccc'
+  },
+  textInput: {
+    borderWidth: 1,
+    width: '70%',
+    margin: 8,
+    padding: 8
+  },
+  goalsContainer: {
+    flex: 5
+  },
+  goalItemContainer: {
+    flex: 1,
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc',
+  },
+  goalItemText: {
+    color: 'white'
+  }
+
 });
