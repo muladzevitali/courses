bbf15faa13fe7f485be770ff731a4a91ea3fcded
Vitali Muladze <muladzevitali@gmail.com>
2024-05-14 10:07:04 +0400

    title

---
+++ react_native/section_4/src/components/ui/Title.ios.js
@@ -0,0 +1,24 @@
+import { Text, StyleSheet } from "react-native"
+
+
+const Title = ({ children }) => {
+    return (
+        <Text style={styles.title}>{children}</Text>
+    )
+}
+
+const styles = StyleSheet.create({
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 24,
+        color: 'white',
+        textAlign: 'center',
+        borderWidth: 0,
+        borderColor: 'white',
+        padding: 12,
+        maxWidth: '70%',
+        width: 300
+    }
+})
+
+export default Title;
